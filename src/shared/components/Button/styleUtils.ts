1d5fba5c7dd6f1169dd7d3ac279e6b7b3a851531
RemainTheSame <34090420+RemainTheSame@users.noreply.github.com>
2025-04-23 20:13:53 +0200

    src/shared/components/: Update components

--- src/shared/components/Button/styleUtils.ts
+++ src/shared/components/Button/styleUtils.ts
@@ -3,9 +3,9 @@ import { StyleSizes } from "./types";
 export const getSizeStyling = (size?: StyleSizes) => {
   switch (size) {
     case "xs":
-      return "text-xs md:text-base w-fit md:w-fit px-4 md:py-2 lg:py-2 md:rounded-md";
+      return "text-xs md:text-base w-fit md:w-fit px-4 py-1 md:py-2 lg:py-2 md:rounded-md";
     case "sm":
-      return "text-sm md:text-lg w-[200px] md:w-[300px] px-4 md:py-3 lg:py-3 md:rounded-lg";
+      return "text-sm md:text-lg w-[200px] md:w-[300px] px-4 py-2 md:py-3 lg:py-3 md:rounded-lg";
     case "md":
       return "";
     case "lg":
