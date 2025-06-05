c9f45a560c2807f309a90e01e66ece1de6ca3474
OKilberg <oscar.oravakilberg@gmail.com>
2025-06-05 23:21:12 +0200

    Logs page with filter started

--- src/features/Logs/Logs.tsx
+++ src/features/Logs/Logs.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+"use client";
+
+import DefaultAppBar from "@/shared/components/AppBar/DefaultAppBar";
+import MainPane from "@/shared/components/MainPane/MainPane";
+import useEmployee from "@/shared/queryState/useEmployee";
+import React, { Suspense } from "react";
+import FilterMenu from "./Components/FilterMenu/FilterMenu";
+import useMonth from "@/shared/queryState/useMonth";
+import useQuery from "@/shared/hooks/useQuery";
+import { getRecordsByUserMonthYear } from "@/lib/db/logs";
+
+const useFilteredLogs = () => {
+  const { employee } = useEmployee();
+  const { month } = useMonth();
+  const year = 2025;
+
+  const filteredLogs = useQuery({
+    fn: () => getRecordsByUserMonthYear(Number(employee), year, Number(month)),
+    key: `logs-${employee}-${month}-${year}`,
+  });
+
+  console.log("filteredLogs", filteredLogs);
+
+  return filteredLogs;
+};
+
+const LogsList = () => {
+  const filteredLogs = useFilteredLogs();
+
+  return (
+    <ol>
+      {filteredLogs.map(({ id, inTime, outTime }) => (
+        <p>{`${id} ${inTime} ${outTime}`}</p>
+      ))}
+    </ol>
+  );
+};
 
 const Logs = () => {
-  return <div>Logs</div>;
+  return (
+    <MainPane className="h-[calc(100vh-3rem)]">
+      <DefaultAppBar
+        pageTitle="Logs"
+        pageDescription="Browse all logs submitted"
+        url="/admin"
+      />
+      <section className="px-4">
+        <FilterMenu />
+        <Suspense
+          fallback={
+            <span className="loading loading-spinner loading-md"></span>
+          }
+        >
+          <LogsList />
+        </Suspense>
+      </section>
+    </MainPane>
+  );
 };
 
 export default Logs;
