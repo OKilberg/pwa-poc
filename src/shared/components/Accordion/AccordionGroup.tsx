94a93e56eb87244c03c41c99e82e95f16fd5f8f4
OKilberg <oscar.oravakilberg@gmail.com>
2025-07-02 21:46:14 +0200

    Use mdx for help articles

--- src/shared/components/Accordion/AccordionGroup.tsx
+++ src/shared/components/Accordion/AccordionGroup.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import React, { createContext } from "react";
+import React, { createContext, useId } from "react";
 import { AccordionGroupProps } from "./types";
 
 export const AccordionContext = createContext<string>("default");
 
-export const AccordionGroup: React.FC<AccordionGroupProps> = ({
-  name,
-  children,
-}) => {
+export const AccordionGroup: React.FC<AccordionGroupProps> = ({ children }) => {
+  const name = useId();
   return (
     <AccordionContext.Provider value={name}>
       <div className="space-y-2">{children}</div>
