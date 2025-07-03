ef780b70a65e42ef1640a75be0d4cac49efb5413
OKilberg <oscar.oravakilberg@gmail.com>
2025-07-03 20:49:01 +0200

    Create Guide accordion component

--- src/features/Help/Components/ManageEmployeesGuides.tsx
+++ src/features/Help/Components/ManageEmployeesGuides.tsx
@@ -1,29 +1,8 @@
 import { AccordionGroup } from "@/shared/components/Accordion/AccordionGroup";
-import AccordionItem from "@/shared/components/Accordion/Subcomponents/AccordionItem";
-import AccordionItemContent from "@/shared/components/Accordion/Subcomponents/AccordionItemContent";
-import AccordionItemTitle from "@/shared/components/Accordion/Subcomponents/AccordionItemTitle";
 import { UserPlus, Users } from "lucide-react";
 import TestArticle from "@/features/Help/Articles/TestArticle.mdx";
-import MdxLayout from "@/shared/components/MdxLayout";
 import CategoryAccordion from "./CategoryAccordion";
-
-const AddEmployee = () => {
-  return (
-    <AccordionItem className="border-b-2 bg-zinc-100">
-      <AccordionItemTitle className="text-md font-medium text-zinc-700">
-        <div className="flex gap-3">
-          <UserPlus size={20} />
-          Add Employee
-        </div>
-      </AccordionItemTitle>
-      <AccordionItemContent>
-        <MdxLayout>
-          <TestArticle />
-        </MdxLayout>
-      </AccordionItemContent>
-    </AccordionItem>
-  );
-};
+import GuideAccordion from "./GuideAccordion";
 
 const ManageEmployeesGuides = () => {
   return (
@@ -31,7 +10,9 @@ const ManageEmployeesGuides = () => {
       <AccordionGroup>
         <CategoryAccordion category="Managing Employees" icon={<Users />}>
           <AccordionGroup>
-            <AddEmployee />
+            <GuideAccordion icon={<UserPlus />} guide={"Add Employee"}>
+              <TestArticle />
+            </GuideAccordion>
           </AccordionGroup>
         </CategoryAccordion>
       </AccordionGroup>
