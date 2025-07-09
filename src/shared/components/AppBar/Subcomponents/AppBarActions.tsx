0b1e9d4d8f723de04815507b611028dab3fee375
OKilberg <oscar.oravakilberg@gmail.com>
2025-07-09 11:12:49 +0200

    src/shared/components/: Topbar now exluded on most paths, logo in appbar instead

--- src/shared/components/AppBar/Subcomponents/AppBarActions.tsx
+++ src/shared/components/AppBar/Subcomponents/AppBarActions.tsx
@@ -4,7 +4,11 @@ import { ParentComponent } from "../../types";
 type AppBarActionsProps = ParentComponent;
 
 const AppBarActions = ({ children }: AppBarActionsProps) => {
-  return <div className="flex gap-6 pt-1 md:pt-0">{children}</div>;
+  return (
+    <div className="flex gap-6 pt-1 md:pt-0 flex-1 justify-between">
+      {children}
+    </div>
+  );
 };
 
 export default AppBarActions;
