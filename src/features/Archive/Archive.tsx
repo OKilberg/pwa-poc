15d31ceb9cac3f49b3ca4ec8d96474834083c6db
OKilberg <oscar.oravakilberg@gmail.com>
2025-10-12 20:25:12 +0200

    Feature - Archive: Add Archive feature

---
+++ src/features/Archive/Archive.tsx
@@ -0,0 +1,42 @@
+import DefaultAppBar from "@/shared/components/AppBar/DefaultAppBar";
+import MainPane from "@/shared/components/MainPane/MainPane";
+import MenuItem from "@/shared/components/MenuItem/MenuItem";
+import { Calendar, ChevronRight, ClipboardList, Users } from "lucide-react";
+
+const Archive = () => {
+  return (
+    <MainPane className="h-[calc(100vh-3rem)] min-h-0">
+      <DefaultAppBar
+        pageTitle={"Archive"}
+        pageDescription="View archive pages"
+        url="/admin"
+      />
+      <section className="flex flex-col flex-1 overflow-y-scroll py-2 px-4 gap-3">
+        <MenuItem
+          linkTo="/admin/calendar?filter=archived"
+          description="View calendar with archived employees"
+          label="Calendar [Archive]"
+          leadingIcon={<Calendar />}
+          trailingIcon={<ChevronRight />}
+        />
+        <MenuItem
+          linkTo="/admin/employees?filter=archived"
+          description="View your archived employees"
+          label="Employees [Archive]"
+          leadingIcon={<Users />}
+          trailingIcon={<ChevronRight />}
+        />
+        <MenuItem
+          linkTo="/admin/logs?filter=archived"
+          description="View and manage work logs"
+          label="Logs [Archive]"
+          leadingIcon={<ClipboardList />}
+          trailingIcon={<ChevronRight />}
+          disabled
+        />
+      </section>
+    </MainPane>
+  );
+};
+
+export default Archive;
